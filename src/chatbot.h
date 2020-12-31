7630bb6d329db631d8f6cb61cf217dcdbae901ea
Vasundhara Jain <vasundhara.jain@daimler.com>
2020-12-31 16:59:21 +0100

    Apply clang format

--- src/chatbot.h
+++ src/chatbot.h
@@ -4,43 +4,27 @@
 #include <wx/bitmap.h>
 #include <string>
 
-class GraphNode; // forward declaration
-class ChatLogic; // forward declaration
+class GraphNode;  // forward declaration
+class ChatLogic;  // forward declaration
 
-class ChatBot
-{
-private:
-    // data handles (owned)
-    wxBitmap *_image; // avatar image
+class ChatBot {
+ private:
+  // data handles (owned)
+  wxBitmap *_image;  // avatar image
 
-    // data handles (not owned)
-    GraphNode *_currentNode;
-    GraphNode *_rootNode;
-    ChatLogic *_chatLogic;
+  // data handles (not owned)
+  GraphNode *_currentNode;
+  GraphNode *_rootNode;
+  ChatLogic *_chatLogic;
 
-    // proprietary functions
-    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+  // proprietary functions
+  int ComputeLevenshteinDistance(std::string s1, std::string s2);
 
-public:
-    // constructors / destructors
-    ChatBot();                     // constructor WITHOUT memory allocation
-    ChatBot(std::string filename); // constructor WITH memory allocation
-    ~ChatBot();
-
-    //// STUDENT CODE
-    ////
-
-    ////
-    //// EOF STUDENT CODE
-
-    // getters / setters
-    void SetCurrentNode(GraphNode *node);
-    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
-    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
-    wxBitmap *GetImageHandle() { return _image; }
-
-    // communication
-    void ReceiveMessageFromUser(std::string message);
+ public:
+  // constructors / destructors
+  ChatBot();                      // constructor WITHOUT memory allocation
+  ChatBot(std::string filename);  // constructor WITH memory allocation
+  ~ChatBot();
   // Assignment constructor
   ChatBot &operator=(const Chatbot &source);
   // Copy constructor
@@ -49,6 +33,21 @@ public:
   ChatBot(MyMovableClass &&source);
   // Move assignment constructor
   Chatbot &operator=(Chatbot &&source);
+
+  //// STUDENT CODE
+  ////
+
+  ////
+  //// EOF STUDENT CODE
+
+  // getters / setters
+  void SetCurrentNode(GraphNode *node);
+  void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+  void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+  wxBitmap *GetImageHandle() { return _image; }
+
+  // communication
+  void ReceiveMessageFromUser(std::string message);
 };
 
 #endif /* CHATBOT_H_ */
